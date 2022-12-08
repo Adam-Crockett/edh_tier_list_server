8962e98844cad1135b34b99ff5914e52d42a4a20
Adam Crockett <arc1224@gmail.com>
2022-12-07 17:11:40 -0800

    added more set fields

--- src/models/set.js
+++ src/models/set.js
@@ -2,9 +2,11 @@ import mongoose from 'mongoose';
 
 const setSchema = mongoose.Schema({
   id: { type: String, required: true },
+  object: { type: String, required: true },
   code: { type: String, required: true },
   name: { type: String, required: true },
   icon_svg_uri: { type: String, required: false },
+  createdAt: { type: Date, default: new Date() },
 });
 
 export default mongoose.model('Set', setSchema);
