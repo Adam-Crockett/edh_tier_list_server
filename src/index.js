f420ed227ad24186aa245df1184adab3cca0a550
Adam Crockett <arc1224@gmail.com>
2022-12-09 15:02:15 -0800

    index clean up

--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ import dotenv from 'dotenv';
 
 import axios from 'axios';
 
+import getSets from './services/getSets.js';
+
 dotenv.config();
 const app = express();
 const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
-
 app.use(cors());
+
+// app.use('/', setRoutes);
+// app.use('/', cardRoutes);
 
 mongoose.connect(CONNECTION_URL, {
   useUnifiedTopology: true,
@@ -24,47 +28,6 @@ db.once('open', () => {
 });
 
 db.on('error', console.error.bind(console, 'MongoDB connection error: '));
-
-async function getSets() {
-  try {
-    const response = await axios.get('https://api.scryfall.com/sets', {
-      responseType: 'json',
-      responseEncoding: 'utf8',
-      headers: {
-        'Accept-Encoding': 'application/json',
-      },
-    });
-
-    const setTypeSet = new Set();
-    const setValidEDH = new Set();
-
-    // masterpiece: only Transformers, funny: only Unfinity,
-
-    const validEDHSetType = [
-      'masters',
-      'commander',
-      'expansion',
-      'draft_innovation',
-      'masterpiece',
-      'funny',
-      'core',
-    ];
-
-    const validSubset = ['Transformers', 'Unfinity'];
-
-    let setCount = 0;
-
-    response.data.data.map((set) => {
-      if (validEDHSetType.includes(set.set_type)) {
-        console.log(set.name, set.id);
-        setCount++;
-      }
-    });
-    console.log(setCount);
-  } catch (error) {
-    console.log(error);
-  }
-}
 
 // Query to find cards that are: from target sets, can be your commander, one of the target sets is the card's first printing.
 // `https://api.scryfall.com/cards/search?order=name&q=legal%3Acommander+%28set%3A${set1}+OR+set%3A${set2}%29+%28is%3Acommander+is%3Afirstprint%29`,
@@ -100,8 +63,8 @@ async function getCommanders(setArray) {
     console.log(error);
   }
 }
-
-// getSets();
+const sets = await getSets();
+console.log(sets.length);
 
 // const setArray = ['59a2059f-5482-433f-8761-eb2e17859b71'];
 
