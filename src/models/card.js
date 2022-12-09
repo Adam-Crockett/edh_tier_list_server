ba17ce0254136d9be60e7eb79336c4f7a853ff12
Adam Crockett <arc1224@gmail.com>
2022-12-08 20:48:21 -0800

    schema updates and research

--- src/models/card.js
+++ src/models/card.js
@@ -1,24 +1,24 @@
 import mongoose from 'mongoose';
 
 const cardSchema = mongoose.Schema({
-  id: { type: String, required: true },
-  set_id: { type: String, required: true },
-  set: { type: String, required: true },
-  object: { type: String, requried: true },
-  legalities: { type: legalitiesObject, required: true },
-  name: { type: String, required: true },
-  card_faces: { type: Object }, // Further implementation; for the sub-cards of this. Address Dual faced cards.
   all_parts: { type: Object }, // Further implementation; for cards like meld commanders and possible others.
+  card_faces: { type: Object }, // Further implementation; for the sub-cards of this. Address Dual faced cards.
+  color_identity: [String],
+  colors: [String],
+  createdAt: { type: Date, default: new Date() },
+  id: { type: String, required: true },
   image_status: { type: String, required: true },
   // Need to pick what images to pick up and what to store vs API reference
+  legalities: { type: legalitiesObject, required: true }, // This may not be needed based on use scope
   mana_cost: String,
-  colors: [String],
-  color_identity: [String],
-  type_line: String,
+  name: { type: String, required: true },
+  object: { type: String, requried: true },
   oracle_text: String,
   power: String,
+  set_id: { type: String, required: true },
+  set: { type: String, required: true },
   toughness: String,
-  createdAt: { type: Date, default: new Date() },
+  type_line: String,
 });
 
 const legalitiesObject = mongoose.Schema({
--- src/models/set.js
+++ src/models/set.js
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
 
 const setSchema = mongoose.Schema({
-  id: { type: String, required: true },
-  object: { type: String, required: true },
   code: { type: String, required: true },
-  name: { type: String, required: true },
-  icon_svg_uri: { type: String, required: false },
   createdAt: { type: Date, default: new Date() },
+  icon_svg_uri: { type: String, required: false },
+  id: { type: String, required: true },
+  name: { type: String, required: true },
+  object: { type: String, required: true },
 });
 
 export default mongoose.model('Set', setSchema);
