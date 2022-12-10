aa57aa0a4d13c143585adb29540a15fbc608aafb
Adam Crockett <arc1224@gmail.com>
2022-12-09 17:23:21 -0800

    index updates for testing db connection

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import express from 'express';
-import mongoose, { set } from 'mongoose';
+import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
 import axios from 'axios';
+import setRoutes from './routes/sets.js';
+import { createSets } from './controllers/sets.js';
 
-import getSets from './services/getSets.js';
+// import getSets from './services/getScryfallSets.js';
 
-dotenv.config();
 const app = express();
+dotenv.config();
 const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 app.use(cors());
@@ -16,15 +18,21 @@ app.use(cors());
 // app.use('/', setRoutes);
 // app.use('/', cardRoutes);
 
-mongoose.connect(CONNECTION_URL, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-});
-
 const db = mongoose.connection;
 
 db.once('open', () => {
+  createSets();
   console.log('MongoDB connection established...');
+});
+
+// app.post('/', setRoutes);
+
+app.use('/', (req, res) => {
+  res.send('APP IS RUNNING');
+});
+mongoose.connect(CONNECTION_URL, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
 });
 
 db.on('error', console.error.bind(console, 'MongoDB connection error: '));
@@ -63,8 +71,8 @@ async function getCommanders(setArray) {
     console.log(error);
   }
 }
-const sets = await getSets();
-console.log(sets.length);
+// const sets = await getSets();
+// console.log(sets.length);
 
 // const setArray = ['59a2059f-5482-433f-8761-eb2e17859b71'];
 
