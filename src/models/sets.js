ff1d3d3ad80a3ed0deebdbcb25ff4a95dce80b0a
Adam Crockett <arc1224@gmail.com>
2023-03-06 16:09:55 -0800

    model testing

--- src/models/sets.js
+++ src/models/sets.js
@@ -1,15 +1,4 @@
 import mongoose from 'mongoose';
-// import setSchema from './set.js';
-
-// const setSchema = mongoose.Schema({
-//   code: { type: String, required: true },
-//   createdAt: { type: Date, default: new Date() },
-//   icon_svg_uri: { type: String, required: false },
-//   id: { type: String, required: true },
-//   name: { type: String, required: true },
-//   object: { type: String, required: true },
-//   released_at: { type: Date },
-// });
 
 const setsSchema = mongoose.Schema({
   createdAt: { type: Date, default: new Date() },
