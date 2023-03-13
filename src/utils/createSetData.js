fb17a6bbf64e76d8632ed6089f341192da9dfb89
Adam Crockett <arc1224@gmail.com>
2023-03-12 22:54:44 -0700

    test to remove unneeded function

--- src/utils/createSetData.js
+++ src/utils/createSetData.js
@@ -2,20 +2,20 @@ import Set from '../models/set.js';
 
 // Possibly delete as sets are now created and baked into periodic update
 const createSetData = async (fetchedSets) => {
-  for (const set of fetchedSets) {
-    const newSet = new Set({
-      code: set.code,
-      createdAt: new Date(),
-      icon_svg_uri: set.icon_svg_uri,
-      id: set.id,
-      name: set.name,
-      object: set.object,
-      released_at: set.released_at,
-      scryfall_uri: set.scryfall_uri,
-      search_uri: set.search_uri,
-    });
-    await newSet.save();
-  }
+  // for (const set of fetchedSets) {
+  //   const newSet = new Set({
+  //     code: set.code,
+  //     createdAt: new Date(),
+  //     icon_svg_uri: set.icon_svg_uri,
+  //     id: set.id,
+  //     name: set.name,
+  //     object: set.object,
+  //     released_at: set.released_at,
+  //     scryfall_uri: set.scryfall_uri,
+  //     search_uri: set.search_uri,
+  //   });
+  //   await newSet.save();
+  // }
   return true;
 };
 
