1bfa3089c59129435bd9f69ef6fbf8fa96fc2e84
Adam Crockett <arc1224@gmail.com>
2023-03-13 11:05:25 -0700

    server sending card data back to client

--- src/controllers/cards.js
+++ src/controllers/cards.js
@@ -4,17 +4,8 @@ import Card from '../models/card.js';
 export const getCards = async (req, res) => {
   if (req.query.setCodes) {
     try {
-      const cardDatas = Card.find(
-        { set: { $in: req.query.setCodes } },
-        (error, cardData) => {
-          if (error) {
-            console.error(error);
-          }
-          return cardData;
-        }
-      );
-      console.log(cardDatas);
-      res.status(200).json(cardDatas);
+      const cardData = await getCardData(req.query.setCodes);
+      res.status(200).json(cardData);
     } catch (error) {
       console.log(error);
       res.status(404).send({ message: 'Error in retrieving cards' });
--- src/dbOperations/getCardData.js
+++ src/dbOperations/getCardData.js
@@ -1,11 +1,7 @@
 import Card from '../models/card.js';
 // Fix to get set Data out of Mongo
-const getCardData = async (setCodes) =>
-  Card.find({ set: { $in: setCodes } }, (error, cardData) => {
-    if (error) {
-      console.error(error);
-    }
-    return cardData;
-  });
+const getCardData = async (setCodes) => {
+  return Card.find({ set: { $in: setCodes } });
+};
 
 export default getCardData;
