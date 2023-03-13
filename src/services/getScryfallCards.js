07c44579972725467396fcb6d8a3458a32beae03
Adam Crockett <arc1224@gmail.com>
2023-03-12 22:54:07 -0700

    api call to scryfall for cards

---
+++ src/services/getScryfallCards.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+export default async function getScryfallCards(setList) {
+  const instance = axios.create({
+    baseURL: 'https://api.scryfall.com',
+    responseType: 'json',
+    responseEncoding: 'utf8',
+    headers: {
+      'Accept-Encoding': 'application/json',
+    },
+  });
+  let completeResult = [];
+  let sucessCount = 0;
+  console.log('Retrieving cards from sets...');
+  for (let set of setList) {
+    try {
+      const res = await instance.get(requestConstructor(set));
+      if (res.status === 200) {
+        completeResult.push(res.data);
+        sucessCount += 1;
+      }
+    } catch (error) {
+      // console.error(`Could not fetch cards from this set: ${set}`);
+    }
+    await new Promise((resolve) => setTimeout(resolve, 100));
+  }
+  console.log(`Cards retrieved from ${sucessCount} sets on scryfall.`);
+  return completeResult;
+}
+
+function requestConstructor(code) {
+  return `https://api.scryfall.com/cards/search?order=name&q=legal%3Acommander+%28set%3A${code}%29+%28is%3Acommander+is%3Afirstprint%29`;
+}
+
+// Query to find cards that are: from target sets, can be your commander, and one of the target sets is the card's first printing.
+// `https://api.scryfall.com/cards/search?order=name&q=legal%3Acommander+%28set%3A${set1}+OR+set%3A${set2}%29+%28is%3Acommander+is%3Afirstprint%29`,
