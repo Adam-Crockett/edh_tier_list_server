810206c67fdf6208a46b25e96301ae9df50f5144
Adam Crockett <arc1224@gmail.com>
2023-04-08 09:44:41 -0700

    turn off cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import cors from 'cors';
+// import cors from 'cors';
 import dotenv from 'dotenv';
 
 import setRoutes from './routes/sets.js';
@@ -14,7 +14,7 @@ const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
-app.use(cors());
+// app.use(cors());
 
 app.use('/sets', setRoutes);
 app.use('/cards', cardRoutes);
